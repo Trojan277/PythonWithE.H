b0ed70acef931ddbc1d5b8c49d3dada591118c8c
Trojan277 <70176627+Trojan277@users.noreply.github.com>
2021-02-24 13:11:40 +0300

    Add files via upload

--- 30-MacAddressChanger.py
+++ 30-MacAddressChanger.py
@@ -1,13 +1,16 @@
 # python 30-MacAddressChanger.py --interface eth0 --mac (type mac address here)
+# python3 30-MacAddressChanger.py -i eth0 -m (type mac address here)
+
+# python2 - python3
 
 import subprocess
 import optparse
 
 parse_object = optparse.OptionParser()
 parse_object.add_option('-i','--interface',dest = 'interface', help='interface to change!')
-parse_object.add_option('m','--mac',dest = 'mac_address',help = 'new mac address')
+parse_object.add_option('-m','--mac',dest = 'mac_address',help = 'new mac address')
 
-(user_inputs,arguments) = parse_object.parse_object()
+(user_inputs,arguments) = parse_object.parse_args()
 
 user_interface = user_inputs.interface
 user_mac_address = user_inputs.mac_address
